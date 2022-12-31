946c273e8376a625858718ac4a21f0aeae168947
BZ <brianbzhang@gmail.com>
2022-12-31 18:13:58 -0500

    update popup menu

--- background/background.js
+++ background/background.js
@@ -1,1 +1,16 @@
 console.log('%c background', 'color: #0bb');
+
+chrome.runtime.onConnect.addListener((port) => {
+  console.log('%c background on connect', 'color: #b0b', port.name);
+  extensionPort = port;
+  port.onMessage.addListener((message) => {
+    console.log('%c port message', 'color: #b0b', message, port);
+    setTimeout(() => {
+      port.postMessage({
+        name: port.name,
+        answer: 'post message received',
+        sender: 'background',
+      });
+    });
+  });
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,44 @@
-console.log('%c popup', 'color: #0bb');
+console.log("%c popup", "color: #0bb");
+
+const port = chrome.runtime.connect({ name: "popup" });
+
+const startButton = document.querySelector("#start-button");
+const stopButton = document.querySelector("#stop-button");
+
+const updateButtonState = () => {
+  const isRunning = JSON.parse(localStorage.getItem("isRunning"));
+  console.log("%c isRunning", "color: #b0b", isRunning);
+  startButton.disabled = isRunning;
+  stopButton.disabled = !isRunning;
+};
+
+const init = () => {
+  updateButtonState();
+
+  port.postMessage({
+    event: "init",
+    source: "popup",
+  });
+
+  startButton.addEventListener("click", () => {
+    console.log("%c popup click start", "color: #b0b", event);
+    localStorage.setItem("isRunning", "true");
+    updateButtonState();
+    port.postMessage({
+      event: "start",
+      source: "start_button",
+    });
+  });
+
+  stopButton.addEventListener("click", () => {
+    console.log("%c popup click stop", "color: #b0b", event);
+    localStorage.setItem("isRunning", "false");
+    updateButtonState();
+    port.postMessage({
+      event: "stop",
+      source: "stop_button",
+    });
+  });
+};
+
+init();
